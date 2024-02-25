c7fc35f47d70218e1e7eb8fa6c8b0bf074e08a01
OlegPanadiy <92667024+olehpanadii@users.noreply.github.com>
2024-02-25 18:57:32 +0000

    routes update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
-import { Route, Routes } from "react-router-dom";
-import { Layout } from "./Layout/Layout";
-import { lazy } from "react";
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout/Layout';
+import { lazy } from 'react';
 
-const HomePage = lazy(() => import("../pages/HomePage"));
-const CatalogPage = lazy(() => import("../pages/CatalogPage"));
-const FavoritePage = lazy(() => import("../pages/FavoritePage"));
+const HomePage = lazy(() => import('../pages/HomePage'));
+const CatalogPage = lazy(() => import('../pages/CatalogPage'));
+const FavoritePage = lazy(() => import('../pages/FavoritePage'));
 
 export const App = () => {
   return (
@@ -15,6 +15,7 @@ export const App = () => {
           <Route path="/catalog" element={<CatalogPage />}></Route>
           <Route path="/favorite" element={<FavoritePage />}></Route>
         </Route>
+        <Route path="*" element={<Navigate to={'/'} />} />
       </Routes>
     </div>
   );
